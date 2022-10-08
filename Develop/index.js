7ffef4d79129ea71513674acf543e92ab1c33339
Chrisld50 <chrisld50@yahoo.com>
2022-10-08 18:52:50 -0500

    created js for each role and employee itself, created the render for html starter, created questions for each role

--- Develop/index.js
+++ Develop/index.js
@@ -1,2 +1,111 @@
-const fs = require('fs')
+
 const inquirer = require('inquirer')
+const Employee = require('Employee')
+const Manager = require('Manager')
+const Intern = require('Intern'); 
+
+
+
+
+const teamQuestion = [
+
+    {
+        type: 'list',
+        name: 'create',
+        message: 'Would you like to add a member or generate team?',
+        choices: ['Add Member', 'Generate Team']
+
+    },
+
+    {
+        type: 'list',
+        name: 'rollchoice',
+        message: 'What member would you like to add to the team?',
+        choices: ['Manager', 'Engineer', 'Intern'],
+
+    },
+
+    {
+        type: 'input',
+        name: 'page',
+        message: 'Please name your team page'
+    }
+];
+
+
+const questions = {
+
+
+Manager: [
+
+{
+    type: 'input',
+    name: 'name',
+    message:'What is your name?'
+},
+{
+    type: 'input',
+    name: 'id',
+    message:'What is your id?'
+},
+{
+    type: 'input',
+    name: 'email',
+    message:'What is your email?'
+},
+{
+    type: 'input',
+    name: 'OfficeNumber',
+    message: 'What is your office number?'
+}
+],
+
+Engineer: [
+
+{
+    type: 'input',
+    name: 'name',
+    message:'What is your name?'
+},
+{
+    type: 'input',
+    name: 'id',
+    message:'What is your id?'
+},
+{
+    type: 'input',
+    name: 'email',
+    message:'What is your email?'
+},
+{
+    type: 'input',
+    name: 'github',
+    message: 'What is your Github?'
+}
+],
+
+Intern: [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is your id?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?'
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'What is the name of the school?'
+    }
+]
+
+
+}
