33dac0a1062c6eb1b07e6727fc97fe34e4df1892
Chrisld50 <chrisld50@yahoo.com>
2022-10-11 22:06:18 -0500

    removed previous way of doing html, added to test files, added to .js of each, created index.js files and renderHTML files.

--- Develop/index.js
+++ Develop/index.js
@@ -1,111 +1,203 @@
-
 const inquirer = require('inquirer')
-const Employee = require('Employee')
-const Manager = require('Manager')
-const Intern = require('Intern'); 
+const Manager = require('./lib/Manager')
+const Intern = require('./lib/Intern')
+const Engineer = require('./lib/Engineer')
+const fs = require('fs')
+const RenderHTML = require('../Develop/lib/RenderHTML')
+
+const teamMembers = []
 
 
 
+init();
 
-const teamQuestion = [
 
-    {
-        type: 'list',
-        name: 'create',
-        message: 'Would you like to add a member or generate team?',
-        choices: ['Add Member', 'Generate Team']
+function init(){
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'userChoice',
+            message: 'Would you like to add a member or generate team?',
+            choices: ['Add Member', 'Generate Team']
+    
+        },
+    ])
+    .then((data) =>{
+        console.log(data)
+        const {userChoice} = data
+       switch (userChoice) {
+        case 'Add Member':
+            _addMember()
+            break;
+        case 'Generate Team':
+            _generateTeam()
+            break;
+        default:
+            break;
+       }
+    })
+    .catch((error) => {console.log(error)})
 
-    },
-
-    {
-        type: 'list',
-        name: 'rollchoice',
-        message: 'What member would you like to add to the team?',
-        choices: ['Manager', 'Engineer', 'Intern'],
-
-    },
-
-    {
-        type: 'input',
-        name: 'page',
-        message: 'Please name your team page'
+    function _generateTeam(){
+        console.log(teamMembers)
+        if(teamMembers.length < 1){
+            console.log('no members in your team')
+        }else{
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'filename',
+                    message: 'what is your filename?'
+                },
+            ])
+            .then(() => {
+                const htmlContent = RenderHTML(data)
+                fs.writeFile(htmlContent, RenderHTML(teamMembers), (err) =>
+                err? console.log(err) : console.log('README created!')) 
+        })
+      }
     }
-];
-
-
-const questions = {
-
-
-Manager: [
-
-{
-    type: 'input',
-    name: 'name',
-    message:'What is your name?'
-},
-{
-    type: 'input',
-    name: 'id',
-    message:'What is your id?'
-},
-{
-    type: 'input',
-    name: 'email',
-    message:'What is your email?'
-},
-{
-    type: 'input',
-    name: 'OfficeNumber',
-    message: 'What is your office number?'
-}
-],
-
-Engineer: [
-
-{
-    type: 'input',
-    name: 'name',
-    message:'What is your name?'
-},
-{
-    type: 'input',
-    name: 'id',
-    message:'What is your id?'
-},
-{
-    type: 'input',
-    name: 'email',
-    message:'What is your email?'
-},
-{
-    type: 'input',
-    name: 'github',
-    message: 'What is your Github?'
-}
-],
-
-Intern: [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is your name?'
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'What is your id?'
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email?'
-    },
-    {
-        type: 'input',
-        name: 'school',
-        message: 'What is the name of the school?'
+   
+    function _addMember() {
+        inquirer.prompt([
+          
+            {
+                type: 'list',
+                name: 'members',
+                message: 'What member would you like to add to the team?',
+                choices: ['Manager', 'Engineer', 'Intern', 'I am finished'],
+        
+            },
+        ])
+        .then((data) =>{
+            console.log(data)
+            const {members} = data
+           switch (members) {
+            case 'Manager':
+                _createManager()
+                break;
+            case 'Engineer':
+                _createEngineer()
+                break;
+            case 'Intern':
+                _createIntern()
+                break;
+           
+            default:
+                break;
+           }
+        })
+        .catch((error) => {console.log(error)})   
     }
-]
-
-
+    
+    
+    function _createManager(){
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message:'What is the manager\'s name?'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message:'What is the manager\'s id?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message:'What is the manager\'s email?'
+            },
+            {
+                type: 'input',
+                name: 'OfficeNumber',
+                message: 'What is the manager\'s office number?'
+            }
+        ])
+        .then((data) => {
+            console.log(data)
+            const {name, id, email, officeNumber} = data
+            const manager = new Manager(name, id, email, officeNumber)
+            teamMembers.push(manager)
+            console.log('team members', teamMembers)
+            init()
+        })
+        .catch((err) => {
+            if(err) throw err;
+        })
+    
+    }
+    
+    function _createEngineer(){
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message:'What is the engineer\'s name?'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message:'What is the engineer\'s id?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message:'What is the engineer\'s email?'
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'What is the engineer\'s Github?'
+            }
+        ])
+        .then((data) => {
+            console.log(data)
+            const {name, id, email, github} = data
+            const engineer = new Engineer(name, id, email, github)
+            teamMembers.push(engineer)
+            console.log('team members', teamMembers)
+            init()
+        })
+        .catch((err) => {
+            if(err) throw err;
+        })
+    }
+    
+    function _createIntern(){
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the intern\'s name?'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the intern\'s id?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the intern\'s email?'
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: 'What is the name of the intern\'s school?'
+            }
+        ])
+        .then((data) => {
+            console.log(data)
+            const {name, id, email, school} = data
+            const intern = new Intern(name, id, email, school)
+            teamMembers.push(intern)
+            console.log('team members', teamMembers)
+            init()
+        })
+        .catch((err) => {
+            if(err) throw err;
+        })
+    }
 }
+
--- Develop/lib/RenderHTML.js
+++ Develop/lib/RenderHTML.js
@@ -1,1 +1,117 @@
-const fs = require('fs')
+const managerCard = function (manager) {
+    return `
+    <div class="card" style="width: 18rem;">
+    <div class="card-body bg-primary text-white">
+      <h2 class="card-name">${manager.name} </h2>
+      <h3 class="card-role fa-regular fa-mug-hot "> Manager </h3>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">ID: ${manager.id} </li>
+      <li class="list-group-item">Email: ${manager.email} </li>
+      <li class="list-group-item">Office Number: ${manager.officeNumber} </li>
+    </ul>
+  </div>
+    `
+}
+
+const engineerCard = function (engineer) {
+    return `
+    <div class="card" style="width: 18rem;">
+    <div class="card-body bg-primary text-white">
+      <h2 class="card-name"> ${engineer.name} </h2>
+      <h3 class="card-role fa-solid fa-glasses"> Engineer </h3>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">ID: ${engineer.id} </li>
+      <li class="list-group-item">Email: ${engineer.email} </li>
+      <li class="list-group-item">Github: ${engineer.github} </li>
+    </ul>
+  </div>
+    `
+}
+
+const internCard = function (intern){
+    return `
+    <div class="card" style="width: 18rem;">
+    <div class="card-body bg-primary text-white">
+      <h2 class="card-name"> ${intern.name} </h2>
+      <h3 class="card-role fas fa-user-graduate"> Intern </h3>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">ID: ${intern.id} </li>
+      <li class="list-group-item">Email: ${intern.email} </li>
+      <li class="list-group-item">School: ${intern.school} </li>
+    </ul>
+  </div>
+    `
+}
+
+RenderHTML = (data) => {
+    
+    teamArray = [];
+
+    for (let i = 0; i < teamArray.length; i++) {
+        const employee = data[i]
+        const role = employee.roleAssign()
+
+        if(role === 'Manager'){
+            const renderedManager = managerCard(employee)
+            teamArray.push(renderedManager)
+        }
+
+        if(role === 'Engineer'){
+            const renderedEngineer = engineerCard(employee)
+            teamArray.push(renderedEngineer)
+        }
+
+        if(role === 'Intern'){
+            const renderedIntern = internCard(employee)
+            teamArray.push(renderedIntern)
+        }
+        
+
+        
+    }
+    const allCards = teamArray.join('')
+    const finalPage = contentPage(allCards)
+    return finalPage;
+}
+
+const contentPage = function (allCards) {
+   return ` <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <title>New Document Type</title>
+</head>
+
+<body>
+    <div class ="container-fluid">
+        <div class ="mb-3 col-12 jumbotron bg-danger" style="height:75px;">
+            <h1 class="text-center text-white"> My Team </h1>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="team-created col-12 d-flex justify-content-center">
+
+        ${allCards}
+                
+            </div>
+         </div>
+    </div>
+
+ 
+</body>
+</html>
+`
+}
+
+
+
+module.exports = RenderHTML;
